c3731ce3a4eba52663da5bce9bbce74387d0b967
aregt <simsekoglu@gmail.com>
2024-07-28 09:00:47 +0300

    Create CustomUser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
 
 # Create your models here.
